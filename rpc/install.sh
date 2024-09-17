2e8fda6c1e9a8db1abc86536e9a4e97c58149e90
Mas Rubai <masrubai@gmail.com>
2024-09-17 11:22:55 +0700

    Update install.sh

--- rpc/install.sh
+++ rpc/install.sh
@@ -5,19 +5,11 @@ mkdir repocket
 cd repocket
 
 docker pull repocket/repocket:latest &
-pid=$!
+# Tunggu hingga proses dengan PID $pid selesai
+wait $pid
 
-log_file="pull.log"
-
-# Tunggu hingga proses selesai atau timeout setelah 2 menit
-timeout 120s wait $pid > $log_file 2>&1
-
-if [ $? -eq 0 ]; then
-  echo "Proses pull selesai"
-  touch rp.env
-  echo "RP_EMAIL=masrubai@gmail.com" > rp.env
-  echo "RP_API_KEY=04f6b7f2-228a-4948-85bf-017151afdbd2" >> rp.env
-  docker run --env-file rp.env -d --restart=always repocket/repocket
-else
-  echo "Proses pull gagal. Lihat log: $log_file"
-fi
+echo "Proses pull selesai"
+touch rp.env
+echo "RP_EMAIL=masrubai@gmail.com" > rp.env
+echo "RP_API_KEY=04f6b7f2-228a-4948-85bf-017151afdbd2" >> rp.env
+docker run --env-file rp.env -d --restart=always repocket/repocket
