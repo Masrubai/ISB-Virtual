4e496d5deda72791f2ff2d3c4a0b1da9d4b98403
Mas Rubai <masrubai@gmail.com>
2024-09-17 12:34:02 +0700

    Update uninstall.sh

--- rpc/uninstall.sh
+++ rpc/uninstall.sh
@@ -1,1 +1,6 @@
-
+docker stop rpc
+docker rm rpc
+docker image rm repocket/repocket:latest
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo rm -rf /var/lib/docker
+echo "delete and remove rpc success"
